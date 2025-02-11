0342c007b9f94997a8adff5d79eeb330f79dce9e
Sonal Singh <sonalsinghh01@gmail.com>
2025-02-12 00:45:21 +0530

    implementing custom Exception handlers

--- src/main/java/com/example/ems/Exception/EmployeeNotFoundException.java
+++ src/main/java/com/example/ems/Exception/EmployeeNotFoundException.java
@@ -1,5 +1,4 @@
 package com.example.ems.Exception;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
--- src/main/java/com/example/ems/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ems/Exception/GlobalExceptionHandler.java
@@ -1,5 +1,4 @@
 package com.example.ems.Exception;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
