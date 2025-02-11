0f01bd5372678634d9ae7d1842c25402197a07e1
Sonal Singh <sonalsinghh01@gmail.com>
2025-02-12 00:44:37 +0530

    adding entity structures

--- src/main/java/com/example/ems/Entity/Employer.java
+++ src/main/java/com/example/ems/Entity/Employer.java
@@ -15,7 +15,6 @@ public class Employer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String address;
     private String contactNumber;
